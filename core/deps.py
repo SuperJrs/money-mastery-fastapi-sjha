6d72c2f69faeef871bc3ba0ae66f81ac1c9b7c5c
SamuelHumasi <samuelhumasi3@gmail.com>
2023-03-02 20:12:39 -0300

    feat: configuracoes conexao db. #1

---
+++ core/deps.py
@@ -0,0 +1,11 @@
+from .database import Database
+
+database = Database()
+
+def get_session():
+    session = database.session()
+    try:
+        yield session
+    finally:
+        session.close()
+    
