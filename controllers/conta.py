3a646cc0a478f5cef234ac1f8947fe33052d871c
SamuelHumasi <samuelhumasi3@gmail.com>
2023-03-07 21:06:33 -0300

    feat: refatoracao das controllers. #1

--- controllers/conta.py
+++ controllers/conta.py
@@ -2,53 +2,32 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from models.conta_model import Conta
 from core.deps import get_session
 from schemas.conta_schema import ContaSchema
+from repository.conta_repo import ContaRepo
 
 
 router = APIRouter(prefix="/conta")
+repo = ContaRepo()
 
 @router.get("/", response_model=list[ContaSchema])
 def obter_contas(db=Depends(get_session)):
-    result = db.query(Conta).all()
-    return result
+    return repo.get_all(db)
 
-@router.post("/", response_model=ContaSchema)
+@router.post("/", response_model=ContaSchema, status_code=status.HTTP_201_CREATED)
 def adicionar_nova_conta(nova_conta: ContaSchema, db = Depends(get_session)):
-    conta = Conta(
-       cpf_proprietario=nova_conta.cpf_proprietario,
-       nome_proprietario=nova_conta.nome_proprietario,
-       dt_nasc_proprietario=nova_conta.dt_nasc_proprietario,
-       email=nova_conta.email,
-       senha=nova_conta.senha,
-       telefone=nova_conta.telefone
-    )
+    return repo.create(nova_conta, db)
 
-    db.add(conta)
-    db.commit()
-
-    return conta
-
-@router.delete("/{cpf}")
+@router.delete("/{cpf}", status_code=status.HTTP_202_ACCEPTED)
 def deletar_conta(cpf: int, db=Depends(get_session)):
+    return repo.destroy(cpf, db)
 
-    conta_a_deletar = db.query(Conta).filter(Conta.cpf_proprietario == cpf).first()
+# @router.get("/{cpf}", response_model=ContaSchema)
+# def obter_conta_cpf(cpf: int, db=Depends(get_session)):
+#     conta_cpf = db.query(Conta).filter(Conta.cpf_proprietario == cpf).first()
 
-    if not conta_a_deletar:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f'A conta {cpf} não foi encontrada'
-        )
+#     if not conta_cpf:
+#         raise HTTPException(
+#             status_code=status.HTTP_404_NOT_FOUND,
+#             detail=f'A conta {cpf} não foi encontrada'
+#         )
     
-    db.delete(conta_a_deletar)
-    db.commit()
-
-@router.get("/{cpf}", response_model=ContaSchema)
-def obter_conta_cpf(cpf: int, db=Depends(get_session)):
-    conta_cpf = db.query(Conta).filter(Conta.cpf_proprietario == cpf).first()
-
-    if not conta_cpf:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f'A conta {cpf} não foi encontrada'
-        )
-    
-    return conta_cpf
+#     return conta_cpf
